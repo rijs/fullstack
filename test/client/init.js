1340e7a923a3f3db3948579748f5484e5ebcf0ec
pemrouz <p3pedram@hotmail.com>
2014-10-05 20:43:37 +0100

    unused file

--- test/client/init.js
+++
@@ -1,9 +0,0 @@
-ripple.on('ready', function(){
-  console.log('ready')
-  var text = '<script src="mocha.js"></script>' +
-             '<script src="chai.js"></script>' +
-             '<script>mocha.setup("bdd")</script>' +
-             '<script src="tests.js"></script>' +
-             '<script>mocha.run()</script>'
-  document.querySelector('#mocha').innerHTML = text
-})
