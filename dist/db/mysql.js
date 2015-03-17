a93fd5904f779ceb78addb5ab30015a8609be390
pemrouz <p3pedram@hotmail.com>
2015-03-17 23:38:07 +0000

    do not mutate original object, triggrgers UPDATE

--- dist/db/mysql.js
+++ dist/db/mysql.js
@@ -64,7 +64,7 @@ mysql.push = function (table, data) {
   con.query(sql, function (err, rows, fields) {
     if (err) return log("push", table, "failed", err);
     log("push", table, "done");
-    p.resolve(data.id = rows.insertId);
+    p.resolve(rows.insertId);
   });
 
   return p;
