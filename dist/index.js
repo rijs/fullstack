9176ef520f3ce2018aee2d37a09bab1dbdf43dcb
pemrouz <p3pedram@hotmail.com>
2016-12-16 15:48:17 +0000

    chore: build dist

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = create;
-
 var _rijs = require('rijs.backpressure');
 
 var _rijs2 = _interopRequireDefault(_rijs);
@@ -49,29 +44,33 @@ var _rijs21 = require('rijs.serve');
 
 var _rijs22 = _interopRequireDefault(_rijs21);
 
-var _rijs23 = require('rijs.needs');
+var _rijs23 = require('rijs.pages');
 
 var _rijs24 = _interopRequireDefault(_rijs23);
 
-var _rijs25 = require('rijs.sync');
+var _rijs25 = require('rijs.needs');
 
 var _rijs26 = _interopRequireDefault(_rijs25);
 
-var _rijs27 = require('rijs.core');
+var _rijs27 = require('rijs.sync');
 
 var _rijs28 = _interopRequireDefault(_rijs27);
 
-var _rijs29 = require('rijs.data');
+var _rijs29 = require('rijs.core');
 
 var _rijs30 = _interopRequireDefault(_rijs29);
 
-var _rijs31 = require('rijs.css');
+var _rijs31 = require('rijs.data');
 
 var _rijs32 = _interopRequireDefault(_rijs31);
 
-var _rijs33 = require('rijs.fn');
+var _rijs33 = require('rijs.css');
 
 var _rijs34 = _interopRequireDefault(_rijs33);
+
+var _rijs35 = require('rijs.fn');
+
+var _rijs36 = _interopRequireDefault(_rijs35);
 
 var _client = require('utilise/client');
 
@@ -82,22 +81,25 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 _client2.default && !window.ripple && create();
 
+module.exports = create;
+
 function create(opts) {
-  var ripple = (0, _rijs28.default)(); // empty base collection of resources
+  var ripple = (0, _rijs30.default)(); // empty base collection of resources
 
   // enrich..
   (0, _rijs8.default)(ripple); // exposes a single instance
-  (0, _rijs30.default)(ripple); // register data types
-  (0, _rijs32.default)(ripple); // register css types
-  (0, _rijs34.default)(ripple); // register fn types
+  (0, _rijs32.default)(ripple); // register data types
+  (0, _rijs34.default)(ripple); // register css types
+  (0, _rijs36.default)(ripple); // register fn types
   (0, _rijs16.default)(ripple); // expose helper functions and constants
   (0, _rijs4.default)(ripple); // invoke web components, fn.call(<el>, data)
-  (0, _rijs24.default)(ripple); // define default attrs for components
+  (0, _rijs26.default)(ripple); // define default attrs for components
   (0, _rijs18.default)(ripple); // preapplies scoped css 
-  (0, _rijs22.default)(ripple, opts); // serve client libraries
   (0, _rijs14.default)(ripple); // loads/saves from/to localstorage
-  (0, _rijs26.default)(ripple, opts); // syncs resources between server/client
+  (0, _rijs28.default)(ripple, opts); // syncs resources between server/client  
   (0, _rijs2.default)(ripple); // restricts broadcast to clients based on need
+  (0, _rijs22.default)(ripple, opts); // serve client libraries
+  (0, _rijs24.default)(ripple, opts); // serve pages directory 
   (0, _rijs12.default)(ripple); // extend components with features
   (0, _rijs6.default)(ripple); // versioning info and time travel
   (0, _rijs10.default)(ripple, opts); // populates sessionid on each connection
